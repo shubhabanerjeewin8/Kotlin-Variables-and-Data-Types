fbc6bfff69a55ccbf58e891c4d2b265a905c853b
Shubha <shubhabanerjeewin8@gmail.com>
2019-04-03 21:43:17 +0530

    Kotlin Variables and Data Types

---
+++ src/Variables&Data_type.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>){
+    var  myNumber = 10  //Int
+    println(myNumber)
+
+    var myDecimal = 1.05 // Float
+    println(myDecimal)
+
+    var myString: String // String (Mutable String)
+    myString = "Hello World"
+    myString = "Hello Kotlin"
+    println(myString)
+
+    val myAnotherString: String // Constant (Immutable String)
+    myAnotherString = "Hi Constant"
+    print(myAnotherString)
+
+}
